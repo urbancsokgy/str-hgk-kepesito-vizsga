9312617067023f85098c9a80316ef406b88e511b
urbancsokgy <69484560+urbancsokgy@users.noreply.github.com>
2021-09-06 12:56:03 +0200

    School schema

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,31 @@
-/**
- * Exercise: make School mongoose model
- */
+const mongoose = require('mongoose')
+
+const SchoolSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true        
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+   
+    classrooms: [string]
+},
+    {
+        timestamps: true
+    },
+    { versionKey: false})
+
+const School = mongoose.model('School', SchoolSchema, 'Schools');
+
+module.exports = School;
+ 
